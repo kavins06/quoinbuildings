460ba3005b8d41c20efe9f578d1d498696c1d0da
kavinnotetaker-svg <kavin.notetaker@gmail.com>
2026-04-07 23:24:26 -0500

    Use official favicon asset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,12 @@ export const metadata: Metadata = {
   title: siteMeta.title,
   description: siteMeta.description,
   icons: {
-    icon: "/favicon.svg",
+    icon: [
+      { url: "/favicon.png", type: "image/png", sizes: "256x256" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
   },
 };
 
